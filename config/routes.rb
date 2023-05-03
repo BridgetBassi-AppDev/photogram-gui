6700b1347b9220721e4a7ebd55dc63622e51066b
Bridget Bassi <bbassi@chicagobooth.edu>
2023-05-03 21:45:16 +0000

    user pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-
+  get("/users", { :controller => "users", :action => "index" })
+  get("/photos", { :controller => "photos", :action => "index" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+  get("/photos/:an_id", { :controller => "photos", :action => "show" })
 
 end
